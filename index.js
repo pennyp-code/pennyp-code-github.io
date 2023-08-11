9c6b0d9f4485713b8d4b143ab49b9a01fd680267
pennyp-code <129231950+pennyp-code@users.noreply.github.com>
2023-08-11 20:32:24 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-        //my first javascript code
+//my first javascript code
         let person = Penny;
         consol.log('person');
+
+        window.addEventListener('scroll', function() {
+                // Get the current scroll position 
+                var scrollPosition = window.scrollY; 
+                // Calculate the new position for the climber 
+                var climberPosition = 50 + scrollPosition * 0.5; 
+                // Adjust the multiplier to control speed 
+                // Update the climber's position 
+                document.getElementById('climber').style.top = climberPosition + 'px'; 
+        });
